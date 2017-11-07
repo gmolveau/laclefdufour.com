74b6e92c49e859fec83aa183bea55f703285bd69
Grégoire MOLVEAU <gr3ggw@gmail.com>
2017-11-07 17:52:38 +0100

    add h1

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,17 @@
    
    Table Of Content
    
-   2. Smooth Scroll
-   3. Scroll Naviagation Background Change with Sticky Navigation
-   4. Mobile Navigation Hide or Collapse on Click
-   5. Scroll To Top
-   6. Typed.js
-   7. Parallax Background
-   8. Portfolio Filtering
-   9. Magnific Popup
-  10. Testimonial Carousel/Slider
-  11. Statistics Counter
-  12. Google Map
-  13. Lazy Load
-  14. Google Analytics
+  01. Smooth Scroll
+  02. Scroll Naviagation Background Change with Sticky Navigation
+  03. Mobile Navigation Hide or Collapse on Click
+  04. Scroll To Top
+  05. Parallax Background
+  06. Portfolio Filtering
+  07. Magnific Popup
+  08. Testimonial Carousel/Slider
+  09. Google Map
+  10. Lazy Load
+  11. Google Analytics
 
 */
 
@@ -24,7 +22,7 @@
 
     jQuery(document).ready(function() {
 
-        /* 2. Smooth Scroll */
+        /* 1. Smooth Scroll */
         $('a.smoth-scroll').on("click", function(e) {
             var anchor = $(this);
             $('html, body').stop().animate({
@@ -34,7 +32,7 @@
         });
 
 
-        /* 3. Scroll Naviagation Background Change with Sticky Navigation */
+        /* 2. Scroll Naviagation Background Change with Sticky Navigation */
         $(window).on('scroll', function() {
             if ($(window).scrollTop() > 100) {
                 $('.header-top-area').addClass('navigation-background');
@@ -46,7 +44,7 @@
 
 
 
-        /* 4. Mobile Navigation Hide or Collapse on Click */
+        /* 3. Mobile Navigation Hide or Collapse on Click */
         $(document).on('click', '.navbar-collapse.in', function(e) {
             if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
                 $(this).collapse('hide');
@@ -58,7 +56,7 @@
         });
 
 
-        /* 5. Scroll To Top */
+        /* 4. Scroll To Top */
         $(window).scroll(function() {
             if ($(this).scrollTop() >= 500) {
                 $('.scroll-to-top').fadeIn();
@@ -74,7 +72,7 @@
         });
 
 
-        /* 7. Parallax Background */
+        /* 5. Parallax Background */
         $(window).stellar({
             responsive: true,
             horizontalScrolling: false,
@@ -84,44 +82,12 @@
         });
 
 
-        /* 8. Portfolio Filtering */
+        /* 6. Portfolio Filtering */
         $('.portfolio-inner').mixItUp({
             selectors: {
                 filter: '.filter-portfolio'
             }
         });
-
-
-        /* 9. Magnific Popup */
-        $('.portfolio-popup').magnificPopup({
-            type: 'image',
-
-            gallery: {
-                enabled: true,
-            },
-            zoom: {
-                enabled: true,
-                duration: 500,
-            },
-
-            image: {
-                markup: '<div class="mfp-figure portfolio-pop-up">' +
-                    '<div class="mfp-close"></div>' +
-                    '<div class="mfp-img"></div>' +
-                    '<div class="mfp-bottom-bar portfolio_title">' +
-                    '<div class="mfp-title"></div>' +
-                    '<div class="mfp-counter"></div>' +
-                    '</div>' +
-                    '</div>',
-
-                titleSrc: function(item) {
-                    return item.el.attr('title');
-                },
-            },
-        });
-
-
-        /* 10. Portfolio Filtering */
         $('.tourisme-inner').mixItUp({
             selectors: {
                 filter: '.filter-tourisme'
@@ -129,7 +95,35 @@
         });
 
 
-        /* 11. Magnific Popup */
+        /* 7. Magnific Popup */
+        $('.portfolio-popup').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true,
+                preload: 0,
+            },
+            zoom: {
+                enabled: true,
+                duration: 500,
+            },
+            image: {
+                markup: `
+                    <div class="mfp-figure tourisme-pop-up">
+                        <div class="mfp-close"></div>
+                        <div class="mfp-img"></div>
+                        <div class="mfp-bottom-bar tourisme_title">
+                            <div class="mfp-title"></div>
+                            <div class="mfp-counter"></div>
+                        </div>
+                    </div>
+                `,
+                titleSrc: function(item) {
+                    return item.el.attr('title');
+                },
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            },
+        });
+
         $('.tourisme-popup').magnificPopup({
             type: 'image',
             gallery: {
@@ -140,7 +134,6 @@
                 enabled: true,
                 duration: 500
             },
-
             image: {
                 markup:
                 `
@@ -161,7 +154,7 @@
         });
 
 
-        /* 12. Testimonial Carousel/Slider */
+        /* 8. Testimonial Carousel/Slider */
 
         $(".testimonial-carousel-list").owlCarousel({
             lazyLoad: true,
@@ -182,7 +175,7 @@
         });
 
 
-        /* 12. Google Map */
+        /* 9. Google Map */
 
         $('#my-address').gMap({
             zoom: 8,
@@ -195,29 +188,27 @@
             }]
         });
 
+        /* 10. Lazy Load */
+        $('img.img-responsive').lazyload();
+
+
+        /* 11. Google Analytics */
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-XXXXXXXX-X', 'auto');
+        ga('send', 'pageview');
+
 
     });
-
-
-    /* 13. Lazy Load */
-    $('img.img-responsive').lazyload();
-
-
-    /* 14. Google Analytics */
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-XXXXXXXX-X', 'auto');
-    ga('send', 'pageview');
-
 
 })(jQuery);
