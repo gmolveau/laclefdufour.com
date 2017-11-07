08633cc5b7d5235070d6216e1dee338deb0ef02b
Grégoire MOLVEAU <gr3ggw@gmail.com>
2017-11-07 17:06:17 +0100

    add compressed images

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
    
    Table Of Content
    
-   1. Preloader
    2. Smooth Scroll
    3. Scroll Naviagation Background Change with Sticky Navigation
    4. Mobile Navigation Hide or Collapse on Click
@@ -16,44 +15,39 @@
   12. Google Map
   13. Lazy Load
   14. Google Analytics
-  15. Multi-Langues
-
 
 */
 
 
-(function ($) {
+(function($) {
     'use strict';
 
-    jQuery(document).ready(function () {
-		
-       /* Smooth Scroll */
+    jQuery(document).ready(function() {
 
-        $('a.smoth-scroll').on("click", function (e) {
+        /* 2. Smooth Scroll */
+        $('a.smoth-scroll').on("click", function(e) {
             var anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top - 50
             }, 1000);
             e.preventDefault();
         });
-	
-       
-       /* Scroll Naviagation Background Change with Sticky Navigation */
-		 
-        $(window).on('scroll', function () {
+
+
+        /* 3. Scroll Naviagation Background Change with Sticky Navigation */
+        $(window).on('scroll', function() {
             if ($(window).scrollTop() > 100) {
                 $('.header-top-area').addClass('navigation-background');
             } else {
                 $('.header-top-area').removeClass('navigation-background');
             }
         });
-		
-		
-		
-		
-       /* Mobile Navigation Hide or Collapse on Click */
-		
-        $(document).on('click', '.navbar-collapse.in', function (e) {
+
+
+
+
+        /* 4. Mobile Navigation Hide or Collapse on Click */
+        $(document).on('click', '.navbar-collapse.in', function(e) {
             if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
                 $(this).collapse('hide');
             }
@@ -62,12 +56,9 @@
             target: '.navbar-collapse',
             offset: 195
         });
-		 
-		
-		
-		
-      /* Scroll To Top */
-		
+
+
+        /* 5. Scroll To Top */
         $(window).scroll(function() {
             if ($(this).scrollTop() >= 500) {
                 $('.scroll-to-top').fadeIn();
@@ -75,16 +66,15 @@
                 $('.scroll-to-top').fadeOut();
             }
         });
-	
-	
-	    $('.scroll-to-top').click(function(){
-		  $('html, body').animate({scrollTop : 0},800);
-		  return false;
-	    });
-        
-		 
-        /* Parallax Background */
+        $('.scroll-to-top').click(function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
 
+
+        /* 7. Parallax Background */
         $(window).stellar({
             responsive: true,
             horizontalScrolling: false,
@@ -93,84 +83,89 @@
             verticalOffset: 0,
         });
 
-        
-		
-		
-        /* Portfolio Filtering */
 
+        /* 8. Portfolio Filtering */
         $('.portfolio-inner').mixItUp({
-          selectors: {
-            filter: '.filter-portfolio'
-          }
+            selectors: {
+                filter: '.filter-portfolio'
+            }
         });
 
 
-       
-        /* Magnific Popup */
-
+        /* 9. Magnific Popup */
         $('.portfolio-popup').magnificPopup({
             type: 'image',
-			
-            gallery: { enabled: true },
-			zoom: { enabled: true,
-			        duration: 500
-					
-          },
-		  
-         image:{
-               markup: '<div class="mfp-figure portfolio-pop-up">'+
-               '<div class="mfp-close"></div>'+
-               '<div class="mfp-img"></div>'+
-               '<div class="mfp-bottom-bar portfolio_title">'+
-               '<div class="mfp-title"></div>'+
-               '<div class="mfp-counter"></div>'+
-               '</div>'+
-               '</div>',
 
-               titleSrc:function(item){
-                return item.el.attr('title');
-              }
-            }
-          });
+            gallery: {
+                enabled: true,
+            },
+            zoom: {
+                enabled: true,
+                duration: 500,
+            },
 
+            image: {
+                markup: '<div class="mfp-figure portfolio-pop-up">' +
+                    '<div class="mfp-close"></div>' +
+                    '<div class="mfp-img"></div>' +
+                    '<div class="mfp-bottom-bar portfolio_title">' +
+                    '<div class="mfp-title"></div>' +
+                    '<div class="mfp-counter"></div>' +
+                    '</div>' +
+                    '</div>',
 
-        /* Portfolio Filtering */
-        $('.tourisme-inner').mixItUp({
-          selectors: {
-            filter: '.filter-tourisme'
-          }
+                titleSrc: function(item) {
+                    return item.el.attr('title');
+                },
+            },
         });
-       
-        /* Magnific Popup */
+
+
+        /* 10. Portfolio Filtering */
+        $('.tourisme-inner').mixItUp({
+            selectors: {
+                filter: '.filter-tourisme'
+            }
+        });
+
+
+        /* 11. Magnific Popup */
         $('.tourisme-popup').magnificPopup({
             type: 'image',
-      
-            gallery: { enabled: true },
-            zoom: { enabled: true,
-              duration: 500
+            gallery: {
+                enabled: true,
+                preload: 0,
             },
-      
-            image:{
-               markup: '<div class="mfp-figure tourisme-pop-up">'+
-               '<div class="mfp-close"></div>'+
-               '<div class="mfp-img"></div>'+
-               '<div class="mfp-bottom-bar tourisme_title">'+
-               '<div class="mfp-title"></div>'+
-               '<div class="mfp-counter"></div>'+
-               '</div>'+
-               '</div>',
+            zoom: {
+                enabled: true,
+                duration: 500
+            },
 
-               titleSrc:function(item){
-                return item.el.attr('title');
-              }
-            }
-          });
+            image: {
+                markup:
+                `
+                    <div class="mfp-figure tourisme-pop-up">
+                        <div class="mfp-close"></div>
+                        <div class="mfp-img"></div>
+                        <div class="mfp-bottom-bar tourisme_title">
+                            <div class="mfp-title"></div>
+                            <div class="mfp-counter"></div>
+                        </div>
+                    </div>
+                `,
+                titleSrc: function(item) {
+                    return item.el.attr('title');
+                },
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            },
+        });
 
 
-
-        /* Testimonial Carousel/Slider */
+        /* 12. Testimonial Carousel/Slider */
 
         $(".testimonial-carousel-list").owlCarousel({
+            lazyLoad: true,
+            loop: true,
             items: 1,
             autoPlay: true,
             stopOnHover: true,
@@ -183,40 +178,46 @@
             itemsMobile: [479, 1],
             autoHeight: true,
             pagination: false,
-            transitionStyle : "backSlide"
+            transitionStyle: "backSlide"
         });
-		
-         /* 12. Google Map */
-		 
-         $('#my-address').gMap({
+
+
+        /* 12. Google Map */
+
+        $('#my-address').gMap({
             zoom: 8,
             scrollwheel: true,
             maptype: 'ROADMAP',
-            markers:[
-              {
+            markers: [{
                 address: "48 La Touche Pichard, 35720 Plesder",
                 html: "<b>Adresse</b>: <br> 48 La Touche Pichard, 35720 Plesder, France",
                 popup: true
-              }
-            ]
-          });
-              
-		   
-            });
+            }]
+        });
 
 
-        /* 13. Lazy Load */
-        $('img.img-responsive').lazyload();
+    });
 
 
-        // 14. Google Analytics
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-XXXXXXXX-X', 'auto');
-        ga('send', 'pageview');
+    /* 13. Lazy Load */
+    $('img.img-responsive').lazyload();
 
 
-   })(jQuery);
+    /* 14. Google Analytics */
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-XXXXXXXX-X', 'auto');
+    ga('send', 'pageview');
+
+
+})(jQuery);
